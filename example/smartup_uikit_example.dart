07c1b8337c7f220888ced69fb98e2eeb1882946f
StratosKyriazidis <stratoskyriazidis@gmail.com>
2023-01-04 10:47:24 +0200

    initial commit

---
+++ example/smartup_uikit_example.dart
@@ -0,0 +1,6 @@
+import 'package:smartup_uikit/smartup_uikit.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/smartup_uikit.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library smartup_uikit;
+
+export 'src/smartup_uikit_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
