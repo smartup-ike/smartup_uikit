fdd208497a2b3cf30e791ffcbe6017c28b0c3521
StratosKyriazidis <stratoskyriazidis@gmail.com>
2023-01-05 13:11:30 +0200

    hovering

--- lib/src/button_with_leading_or_trailing_icon.dart
+++ lib/src/button_with_leading_or_trailing_icon.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:smartup_uikit/src/helpers/uikit_color_scheme.dart';
-import 'package:smartup_uikit/src/helpers/uikit_states.dart';
+import 'helpers/uikit_color_scheme.dart';
+import 'helpers/uikit_states.dart';
 import 'helpers/size_scheme.dart';
 import 'uikit_icon_theme.dart';
 
@@ -44,114 +44,127 @@ class _ButtonWithLeadingOrTrailingIconState
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: widget.onTap,
-      onTapDown: (_) {
-        if (widget.onTap != null) {
-          setState(() {
-            isPressed = true;
-          });
-        }
-      },
-      onTapUp: (_) {
-        if (widget.onTap != null) {
-          setState(() {
-            isPressed = false;
-          });
-        }
-      },
-      onTapCancel: () {
-        if (widget.onTap != null) {
-          setState(() {
-            isPressed = false;
-          });
-        }
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(
-            strokeAlign: StrokeAlign.outside,
-            width: 2,
-            color: isPressed ? Colors.white : Colors.transparent,
-          ),
-        ),
+    return MouseRegion(
+      onEnter: (_) => setState(
+        () => state = widget.onTap == null
+            ? UIKitState.disabledState
+            : UIKitState.hoverState,
+      ),
+      onExit: (_) => setState(
+        () => state = widget.onTap == null
+            ? UIKitState.disabledState
+            : UIKitState.defaultState,
+      ),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        onTapDown: (_) {
+          if (widget.onTap != null) {
+            setState(() {
+              isPressed = true;
+            });
+          }
+        },
+        onTapUp: (_) {
+          if (widget.onTap != null) {
+            setState(() {
+              isPressed = false;
+            });
+          }
+        },
+        onTapCancel: () {
+          if (widget.onTap != null) {
+            setState(() {
+              isPressed = false;
+            });
+          }
+        },
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 200),
-          alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: isPressed ? Colors.white : Colors.transparent,
             borderRadius: BorderRadius.circular(8),
             border: Border.all(
               strokeAlign: StrokeAlign.outside,
-              width: 1,
+              width: 2,
               color: isPressed ? Colors.white : Colors.transparent,
             ),
-            boxShadow: widget.onTap == null || !(widget.hasShadow ?? true)
-                ? []
-                : isPressed
-                    ? [
-                        BoxShadow(
-                          offset: const Offset(0, 0),
-                          blurRadius: 4,
-                          spreadRadius: 0,
-                          color: const Color(0xFF0D1556).withOpacity(.75),
-                        ),
-                      ]
-                    : [
-                        BoxShadow(
-                          offset: const Offset(0, 1),
-                          blurRadius: 2,
-                          spreadRadius: 0,
-                          color: const Color(0xFF0D1556).withOpacity(.33),
-                        ),
-                        BoxShadow(
-                          offset: const Offset(0, 4),
-                          blurRadius: 8,
-                          spreadRadius: 0,
-                          color: const Color(0xFF201E8D).withOpacity(.25),
-                        ),
-                      ],
           ),
-          height: widget.sizeScheme?.height,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (!widget.removePadding!)
-                const SizedBox(
-                  width: 10,
-                ),
-              if (widget.leading != null) ...[
-                UIKitIconTheme(
-                  color: Colors.black,
-                  size: widget.sizeScheme?.height,
-                  child: widget.leading ?? Container(),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-              ],
-              DefaultTextStyle(
-                  style: widget.sizeScheme?.labelTextStyle ?? const TextStyle(),
-                  child: widget.labelText ?? const Text('Button')),
-              if (!widget.removePadding!)
-                const SizedBox(
-                  width: 10,
-                ),
-              if (widget.trailing != null) ...[
-                UIKitIconTheme(
-                  color: Colors.black,
-                  size: widget.sizeScheme?.height,
-                  child: widget.trailing!,
-                ),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: isPressed ? Colors.white : Colors.transparent,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                strokeAlign: StrokeAlign.outside,
+                width: 1,
+                color: isPressed ? Colors.white : Colors.transparent,
+              ),
+              boxShadow: widget.onTap == null || !(widget.hasShadow ?? true)
+                  ? []
+                  : isPressed
+                      ? [
+                          BoxShadow(
+                            offset: const Offset(0, 0),
+                            blurRadius: 4,
+                            spreadRadius: 0,
+                            color: const Color(0xFF0D1556).withOpacity(.75),
+                          ),
+                        ]
+                      : [
+                          BoxShadow(
+                            offset: const Offset(0, 1),
+                            blurRadius: 2,
+                            spreadRadius: 0,
+                            color: const Color(0xFF0D1556).withOpacity(.33),
+                          ),
+                          BoxShadow(
+                            offset: const Offset(0, 4),
+                            blurRadius: 8,
+                            spreadRadius: 0,
+                            color: const Color(0xFF201E8D).withOpacity(.25),
+                          ),
+                        ],
+            ),
+            height: widget.sizeScheme?.height,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
                 if (!widget.removePadding!)
                   const SizedBox(
                     width: 10,
                   ),
+                if (widget.leading != null) ...[
+                  UIKitIconTheme(
+                    color: Colors.black,
+                    size: widget.sizeScheme?.height,
+                    child: widget.leading ?? Container(),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                ],
+                DefaultTextStyle(
+                    style:
+                        widget.sizeScheme?.labelTextStyle ?? const TextStyle(),
+                    child: widget.labelText ?? const Text('Button')),
+                if (!widget.removePadding!)
+                  const SizedBox(
+                    width: 10,
+                  ),
+                if (widget.trailing != null) ...[
+                  UIKitIconTheme(
+                    color: Colors.black,
+                    size: widget.sizeScheme?.height,
+                    child: widget.trailing!,
+                  ),
+                  if (!widget.removePadding!)
+                    const SizedBox(
+                      width: 10,
+                    ),
+                ],
               ],
-            ],
+            ),
           ),
         ),
       ),
