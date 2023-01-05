ea731158b40c8a5ba7b6dca25c1b5316268f63e5
vvlado23 <43526651+vvlado23@users.noreply.github.com>
2023-01-05 14:19:54 +0200

    clean up example main

--- lib/smartup_uikit.dart
+++ lib/smartup_uikit.dart
@@ -1,7 +1,16 @@
 library smartup_uikit;
 
+import 'package:smartup_uikit/src/button_with_leading_or_trailing_icon.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class SmartupUIKit {
+  ButtonWithLeadingOrTrailingIcon button =
+      const ButtonWithLeadingOrTrailingIcon(
+    onTap: null,
+  );
 }
