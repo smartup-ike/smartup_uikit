60463c396d6ecfb95113f0c810aa8a822917e1a0
StratosKyriazidis <stratoskyriazidis@gmail.com>
2023-01-10 11:30:59 +0200

    radio group widget

---
+++ lib/src/uikit_radio_group.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'uikit_radio_button.dart';
+import 'helpers/size_scheme.dart';
+import 'helpers/uikit_color_scheme.dart';
+
+class UIKitRadioGroup extends HookWidget {
+  const UIKitRadioGroup({
+    super.key,
+    this.label,
+    this.sizeScheme,
+    this.colorScheme,
+    this.optionLabels,
+    this.isVertical,
+  });
+
+  final Widget? label;
+  final SizeScheme? sizeScheme;
+  final UIKitColorScheme? colorScheme;
+  final List<Widget>? optionLabels;
+  final bool? isVertical;
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedIndex$ = useState<int?>(null);
+
+    return Column(
+      children: [
+        label ?? const SizedBox(),
+        Wrap(
+          children: [
+            for (Widget element in optionLabels ?? []) ...[
+              Row(
+                children: [
+                  UIKitRadioButton(
+                    isSelected:
+                        selectedIndex$.value == optionLabels?.indexOf(element),
+                    onTap: () {
+                      selectedIndex$.value = optionLabels?.indexOf(element);
+                    },
+                    colorScheme: colorScheme,
+                  ),
+                  const SizedBox(width: 6),
+                  element,
+                ],
+              ),
+            ],
+          ],
+        )
+      ],
+    );
+  }
+}
