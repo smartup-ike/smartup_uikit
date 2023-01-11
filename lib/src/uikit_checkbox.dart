64fd29231184f211888adfd30ad09ea5cc03e206
StratosKyriazidis <stratoskyriazidis@gmail.com>
2023-01-11 12:50:36 +0200

    checkbox implementation

--- lib/src/uikit_checkbox.dart
+++ lib/src/uikit_checkbox.dart
@@ -1,11 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'helpers/uikit_color_scheme.dart';
+import 'helpers/uikit_size_scheme.dart';
+import 'helpers/uikit_states.dart';
 
 class UIKitCheckbox extends HookWidget {
-  const UIKitCheckbox({super.key});
+  const UIKitCheckbox({
+    super.key,
+    this.icon,
+    this.onTap,
+    this.isChecked,
+    this.colorScheme,
+    this.sizeScheme,
+  });
+
+  final Widget? icon;
+  final VoidCallback? onTap;
+  final bool? isChecked;
+  final UIKitColorScheme? colorScheme;
+  final UIKitSizeScheme? sizeScheme;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final state$ = useState<UIKitState>(
+        onTap == null ? UIKitState.disabled : UIKitState.defaultState);
+    final isHovered$ = useState(false);
+
+    Color? backgroundColor;
+    Color? contentColor;
+    Color? borderColor;
+
+    switch (state$.value) {
+      case UIKitState.defaultState:
+        backgroundColor = colorScheme?.defaultBackgroundColor;
+        contentColor = colorScheme?.defaultContentColor;
+        borderColor = colorScheme?.defaultBorderColor;
+        break;
+      case UIKitState.hover:
+        backgroundColor = colorScheme?.hoverBackgroundColor;
+        contentColor = colorScheme?.hoverContentColor;
+        borderColor = colorScheme?.hoverBorderColor;
+        break;
+      case UIKitState.focused:
+        backgroundColor = colorScheme?.focusedBackgroundColor;
+        contentColor = colorScheme?.focusedContentColor;
+        borderColor = colorScheme?.focusedBorderColor;
+        break;
+      case UIKitState.active:
+        backgroundColor = colorScheme?.activeBackgroundColor;
+        contentColor = colorScheme?.activeContentColor;
+        borderColor = colorScheme?.activeBorderColor;
+        break;
+      case UIKitState.disabled:
+        backgroundColor = colorScheme?.disabledBackgroundColor;
+        contentColor = colorScheme?.disabledContentColor;
+        borderColor = colorScheme?.disabledBorderColor;
+        break;
+      default:
+        break;
+    }
+
+    return Center(
+      child: MouseRegion(
+        cursor: state$.value == UIKitState.disabled
+            ? SystemMouseCursors.basic
+            : SystemMouseCursors.click,
+        onHover: (_) {
+          if (onTap != null) {
+            state$.value = UIKitState.hover;
+            isHovered$.value = true;
+          }
+        },
+        onExit: (_) {
+          if (onTap != null) {
+            state$.value = UIKitState.defaultState;
+            isHovered$.value = false;
+          }
+        },
+        child: GestureDetector(
+          onTap: onTap,
+          onTapDown: (_) {
+            if (onTap != null) {
+              state$.value = UIKitState.focused;
+            }
+          },
+          onTapUp: (_) {
+            if (onTap != null) {
+              state$.value =
+                  isHovered$.value ? UIKitState.hover : UIKitState.defaultState;
+            }
+          },
+          onTapCancel: () {
+            if (onTap != null) {
+              state$.value =
+                  isHovered$.value ? UIKitState.hover : UIKitState.defaultState;
+            }
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(
+                strokeAlign: StrokeAlign.outside,
+                width: 1,
+                color: borderColor ?? Colors.transparent,
+              ),
+            ),
+            height: sizeScheme?.height,
+            width: sizeScheme?.height,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color:
+                    isChecked ?? false ? backgroundColor : Colors.transparent,
+              ),
+              child: isChecked ?? false ? icon : null,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
