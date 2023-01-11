5af12dd062fe1fd2d81e68a75dcee4b423c2e19a
vvlado23 <43526651+vvlado23@users.noreply.github.com>
2023-01-11 15:37:50 +0200

    adds button themes

--- lib/src/theme/su_button_theme_data.dart
+++ lib/src/theme/su_button_theme_data.dart
@@ -4,33 +4,69 @@ import 'package:smartup_uikit/smartup_uikit.dart';
 @immutable
 class SUButtonThemeData {
   final UIKitColorScheme primaryColorScheme;
+  final UIKitColorScheme secondaryColorScheme;
+  final UIKitColorScheme secondaryWhiteColorScheme;
+  final UIKitColorScheme tertiaryColorScheme;
+  final UIKitColorScheme outlineColorScheme;
+  final UIKitColorScheme ghostColorScheme;
   final Typography typography;
 
   const SUButtonThemeData.raw({
     required this.primaryColorScheme,
+    required this.secondaryColorScheme,
+    required this.secondaryWhiteColorScheme,
+    required this.tertiaryColorScheme,
+    required this.outlineColorScheme,
+    required this.ghostColorScheme,
     required this.typography,
   });
 
   factory SUButtonThemeData({
     UIKitColorScheme? primaryColorScheme,
+    UIKitColorScheme? secondaryColorScheme,
+    UIKitColorScheme? secondaryWhiteColorScheme,
+    UIKitColorScheme? tertiaryColorScheme,
+    UIKitColorScheme? outlineColorScheme,
+    UIKitColorScheme? ghostColorScheme,
     Typography? typography,
   }) {
     primaryColorScheme ??= UIKitColorScheme.defaultScheme();
+    secondaryColorScheme ??= UIKitColorScheme.defaultScheme();
+    secondaryWhiteColorScheme ??= UIKitColorScheme.defaultScheme();
+    tertiaryColorScheme ??= UIKitColorScheme.defaultScheme();
+    outlineColorScheme ??= UIKitColorScheme.defaultScheme();
+    ghostColorScheme ??= UIKitColorScheme.defaultScheme();
     typography ??= Typography();
 
     return SUButtonThemeData.raw(
       primaryColorScheme: primaryColorScheme,
+      secondaryColorScheme: secondaryColorScheme,
+      secondaryWhiteColorScheme: secondaryWhiteColorScheme,
+      tertiaryColorScheme: tertiaryColorScheme,
+      outlineColorScheme: outlineColorScheme,
+      ghostColorScheme: ghostColorScheme,
       typography: typography,
     );
   }
 
   SUButtonThemeData copyWith({
-    Typography? typography,
     UIKitColorScheme? primaryColorScheme,
+    UIKitColorScheme? secondaryColorScheme,
+    UIKitColorScheme? secondaryWhiteColorScheme,
+    UIKitColorScheme? tertiaryColorScheme,
+    UIKitColorScheme? outlineColorScheme,
+    UIKitColorScheme? ghostColorScheme,
+    Typography? typography,
   }) {
     return SUButtonThemeData.raw(
-      typography: this.typography,
       primaryColorScheme: primaryColorScheme ?? this.primaryColorScheme,
+      secondaryColorScheme: secondaryColorScheme ?? this.secondaryColorScheme,
+      secondaryWhiteColorScheme:
+          secondaryWhiteColorScheme ?? this.secondaryWhiteColorScheme,
+      tertiaryColorScheme: tertiaryColorScheme ?? this.tertiaryColorScheme,
+      outlineColorScheme: outlineColorScheme ?? this.outlineColorScheme,
+      ghostColorScheme: ghostColorScheme ?? this.ghostColorScheme,
+      typography: this.typography,
     );
   }
 }
