e2202e97cd5aa934eaa095e4a22e6d46ec509a25
StratosKyriazidis <stratoskyriazidis@gmail.com>
2023-01-11 12:51:18 +0200

    adds things to example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -32,44 +32,48 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _isToggleActive = false;
+  bool _isCheckBoxChecked = false;
+  int? _selectedRadioItem;
 
   @override
   Widget build(BuildContext context) {
     return SUTheme(
       data: SUThemeData(
-          buttonThemeData: SUButtonThemeData(
-              colorScheme:
-                  UIKitColorScheme(defaultBackgroundColor: Colors.red))),
+        buttonThemeData: SUButtonThemeData(
+          colorScheme: UIKitColorScheme(
+            defaultBackgroundColor: Colors.black,
+            hoverBackgroundColor: Colors.blue,
+            focusedBackgroundColor: Colors.purple,
+            activeBackgroundColor: Colors.teal,
+            disabledBackgroundColor: Colors.grey,
+            defaultContentColor: Colors.white,
+            hoverContentColor: Colors.green,
+            focusedContentColor: Colors.red,
+            activeContentColor: Colors.orange,
+            disabledContentColor: Colors.grey,
+            defaultBorderColor: Colors.black,
+            hoverBorderColor: Colors.green,
+            focusedBorderColor: Colors.red,
+            activeBorderColor: Colors.orange,
+            disabledBorderColor: Colors.grey,
+          ),
+        ),
+      ),
       child: Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
         body: ListView(
+          padding: const EdgeInsets.all(16),
           children: [
             const SizedBox(height: 8),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                UIKitRadioButton(
-                  isSelected: true,
+                UIKitButton(
+                  labelText: const Text('Button'),
+                  removePadding: false,
                   onTap: () {},
-                  colorScheme: UIKitColorScheme(
-                    defaultBackgroundColor: Colors.white,
-                    hoverBackgroundColor: Colors.white,
-                    focusedBackgroundColor: Colors.white,
-                    activeBackgroundColor: Colors.white,
-                    disabledBackgroundColor: Colors.white,
-                    defaultContentColor: Colors.black,
-                    hoverContentColor: Colors.green,
-                    focusedContentColor: Colors.red,
-                    activeContentColor: Colors.orange,
-                    disabledContentColor: Colors.grey,
-                    defaultBorderColor: Colors.black,
-                    hoverBorderColor: Colors.green,
-                    focusedBorderColor: Colors.red,
-                    activeBorderColor: Colors.orange,
-                    disabledBorderColor: Colors.grey,
-                  ),
                 ),
               ],
             ),
@@ -106,10 +110,10 @@ class _MyHomePageState extends State<MyHomePage> {
               label: const Text('Label'),
               assistiveText: const Text('Assistive text'),
               optionLabels: const [
-                Text('Test for testing'),
-                Text('Test for testing'),
-                Text('Test for testing'),
-                Text('Test for testing'),
+                Text('Test 1'),
+                Text('Test 2'),
+                Text('Test 3'),
+                Text('Test 4'),
               ],
               colorScheme: UIKitColorScheme(
                 defaultBackgroundColor: Colors.white,
@@ -128,6 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 activeBorderColor: Colors.orange,
                 disabledBorderColor: Colors.grey,
               ),
+              onSelect: (index) => setState(() => _selectedRadioItem = index),
             ),
             const SizedBox(height: 8),
             UIKitToggleSwitch(
@@ -150,6 +155,36 @@ class _MyHomePageState extends State<MyHomePage> {
                 focusedBorderColor: Colors.red,
                 activeBorderColor: Colors.orange,
                 disabledBorderColor: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 8),
+            UIKitCheckbox(
+              isChecked: _isCheckBoxChecked,
+              onTap: () =>
+                  setState(() => _isCheckBoxChecked = !_isCheckBoxChecked),
+              sizeScheme: UIKitSizeScheme(
+                height: 16,
+              ),
+              colorScheme: UIKitColorScheme(
+                defaultBackgroundColor: Colors.tealAccent,
+                hoverBackgroundColor: Colors.green,
+                focusedBackgroundColor: Colors.red,
+                activeBackgroundColor: Colors.orange,
+                disabledBackgroundColor: Colors.grey,
+                defaultContentColor: Colors.white,
+                hoverContentColor: Colors.white,
+                focusedContentColor: Colors.white,
+                activeContentColor: Colors.white,
+                disabledContentColor: Colors.white,
+                defaultBorderColor: Colors.black,
+                hoverBorderColor: Colors.green,
+                focusedBorderColor: Colors.red,
+                activeBorderColor: Colors.orange,
+                disabledBorderColor: Colors.grey,
+              ),
+              icon: const Icon(
+                Icons.check,
+                size: 12,
               ),
             ),
           ],
