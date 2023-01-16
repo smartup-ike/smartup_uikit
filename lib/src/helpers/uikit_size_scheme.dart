9bf0595702af318e48b431a1f924b6467aef8195
StratosKyriazidis <stratoskyriazidis@gmail.com>
2023-01-16 13:20:37 +0200

    changes things

--- lib/src/helpers/uikit_size_scheme.dart
+++ lib/src/helpers/uikit_size_scheme.dart
@@ -5,25 +5,34 @@ import 'package:smartup_uikit/src/uikit_icon_theme.dart';
 class UIKitSizeScheme {
   factory UIKitSizeScheme({
     double? height,
+    double? width,
     double? iconSize,
     double? borderSize,
+    double? borderRadius,
     double? pressedBorderSize,
+    EdgeInsets? padding,
     TextStyle? labelTextStyle,
   }) {
     return UIKitSizeScheme._(
       height: height,
+      width: width,
       iconSize: iconSize,
       borderSize: borderSize,
+      borderRadius: borderRadius,
       pressedBorderSize: pressedBorderSize,
+      padding: padding,
       labelTextStyle: labelTextStyle,
     );
   }
 
   UIKitSizeScheme._({
     this.height,
+    this.width,
     this.iconSize,
     this.borderSize,
+    this.borderRadius,
     this.pressedBorderSize,
+    this.padding,
     this.labelTextStyle,
   });
 
@@ -31,6 +40,7 @@ class UIKitSizeScheme {
     height = 40;
     iconSize = UIKitIconSize.size16;
     borderSize = 1;
+    borderRadius = 4;
     pressedBorderSize = 2;
     labelTextStyle = GoogleFonts.getFont(
       'Open Sans',
@@ -44,23 +54,32 @@ class UIKitSizeScheme {
   /// the new values.
   UIKitSizeScheme copyWith({
     double? height,
+    double? width,
     double? iconSize,
     double? borderSize,
+    double? borderRadius,
     double? pressedBorderSize,
+    EdgeInsets? padding,
     TextStyle? labelTextStyle,
   }) {
     return UIKitSizeScheme._(
       height: height ?? this.height,
+      width: width ?? this.width,
       iconSize: iconSize ?? this.iconSize,
       borderSize: borderSize ?? this.borderSize,
+      borderRadius: borderRadius ?? this.borderRadius,
       pressedBorderSize: pressedBorderSize ?? this.pressedBorderSize,
+      padding: padding ?? this.padding,
       labelTextStyle: labelTextStyle ?? this.labelTextStyle,
     );
   }
 
   double? height;
+  double? width;
   double? iconSize;
   double? borderSize;
+  double? borderRadius;
   double? pressedBorderSize;
+  EdgeInsets? padding;
   TextStyle? labelTextStyle;
 }
