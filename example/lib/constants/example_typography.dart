06578563fb750e76ccfcccd5f299d22e3bf38589
StratosKyriazidis <git config --get-all user.name>
2023-02-05 12:09:41 +0200

    adds example colors and typography

---
+++ example/lib/constants/example_typography.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ExampleTypography {
+  static TextStyle get headings1Bold => const TextStyle();
+  static TextStyle get headings1Regular => const TextStyle();
+  static TextStyle get headings2Bold => const TextStyle();
+  static TextStyle get headings2Regular => const TextStyle();
+  static TextStyle get headings3Bold => const TextStyle();
+  static TextStyle get headings3Regular => const TextStyle();
+  static TextStyle get headings4Bold => const TextStyle();
+  static TextStyle get headings4Regular => const TextStyle();
+  static TextStyle get headings5Bold => const TextStyle();
+  static TextStyle get headings5Regular => const TextStyle();
+  static TextStyle get body1Bold => const TextStyle();
+  static TextStyle get body1SemiBold => const TextStyle();
+  static TextStyle get body1Regular => const TextStyle();
+  static TextStyle get body2Bold => const TextStyle();
+  static TextStyle get body2SemiBold => const TextStyle();
+  static TextStyle get body2Regular => const TextStyle();
+  static TextStyle get body3Bold => const TextStyle();
+  static TextStyle get body3SemiBold => const TextStyle();
+  static TextStyle get body3Regular => const TextStyle();
+  static TextStyle get bodySBold => const TextStyle();
+  static TextStyle get bodySSemiBold => const TextStyle();
+  static TextStyle get bodySRegular => const TextStyle();
+  static TextStyle get bodyXSXBold => const TextStyle();
+  static TextStyle get bodyXSBold => const TextStyle();
+  static TextStyle get bodyXSSemiBold => const TextStyle();
+  static TextStyle get bodyXSRegular => const TextStyle();
+  static TextStyle get buttonsLarge => const TextStyle();
+  static TextStyle get buttonsMedium => const TextStyle();
+  static TextStyle get buttonsSmall => const TextStyle();
+}
