59b697ec87f48fd5842b423e19df929de611a24b
StratosKyriazidis <git config --get-all user.name>
2023-02-05 19:12:08 +0200

    adds fields

--- example/lib/constants/example_typography.dart
+++ example/lib/constants/example_typography.dart
@@ -1,33 +1,208 @@
 import 'package:flutter/material.dart';
 
 class ExampleTypography {
-  static TextStyle get headings1Bold => const TextStyle();
-  static TextStyle get headings1Regular => const TextStyle();
-  static TextStyle get headings2Bold => const TextStyle();
-  static TextStyle get headings2Regular => const TextStyle();
-  static TextStyle get headings3Bold => const TextStyle();
-  static TextStyle get headings3Regular => const TextStyle();
-  static TextStyle get headings4Bold => const TextStyle();
-  static TextStyle get headings4Regular => const TextStyle();
-  static TextStyle get headings5Bold => const TextStyle();
-  static TextStyle get headings5Regular => const TextStyle();
-  static TextStyle get body1Bold => const TextStyle();
-  static TextStyle get body1SemiBold => const TextStyle();
-  static TextStyle get body1Regular => const TextStyle();
-  static TextStyle get body2Bold => const TextStyle();
-  static TextStyle get body2SemiBold => const TextStyle();
-  static TextStyle get body2Regular => const TextStyle();
-  static TextStyle get body3Bold => const TextStyle();
-  static TextStyle get body3SemiBold => const TextStyle();
-  static TextStyle get body3Regular => const TextStyle();
-  static TextStyle get bodySBold => const TextStyle();
-  static TextStyle get bodySSemiBold => const TextStyle();
-  static TextStyle get bodySRegular => const TextStyle();
-  static TextStyle get bodyXSXBold => const TextStyle();
-  static TextStyle get bodyXSBold => const TextStyle();
-  static TextStyle get bodyXSSemiBold => const TextStyle();
-  static TextStyle get bodyXSRegular => const TextStyle();
-  static TextStyle get buttonsLarge => const TextStyle();
-  static TextStyle get buttonsMedium => const TextStyle();
-  static TextStyle get buttonsSmall => const TextStyle();
+  static String get fontFamily => 'Open Sans';
+  static TextStyle get headings1Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 38,
+        height: 1.48,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings1Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 38,
+        height: 1.48,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings2Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 30,
+        height: 1.48,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings2Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 30,
+        height: 1.48,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings3Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 24,
+        height: 1.5,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings3Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 24,
+        height: 1.5,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings4Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 20,
+        height: 1.5,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings4Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 20,
+        height: 1.5,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings5Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 18,
+        height: 1.36,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get headings5Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 18,
+        height: 1.36,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get body1Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 16,
+        height: 1.36,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get body1SemiBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
+        height: 1.36,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get body1Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+        height: 1.36,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get body2Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 14,
+        height: 1.4,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get body2SemiBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 14,
+        height: 1.4,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get body2Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 14,
+        height: 1.4,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get body3Bold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 12,
+        height: 1.36,
+        letterSpacing: -0.01,
+      );
+  static TextStyle get body3SemiBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 12,
+        height: 1.36,
+        letterSpacing: -0.01,
+      );
+  static TextStyle get body3Regular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 12,
+        height: 1.36,
+        letterSpacing: -0.01,
+      );
+  static TextStyle get bodySBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 11,
+        height: 1.46,
+        letterSpacing: 0,
+      );
+  static TextStyle get bodySSemiBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 11,
+        height: 1.46,
+        letterSpacing: 0,
+      );
+  static TextStyle get bodySRegular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 11,
+        height: 1.46,
+        letterSpacing: 0,
+      );
+  static TextStyle get bodyXSXBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w800,
+        fontSize: 10,
+        height: 1.36,
+        letterSpacing: 0.08,
+      );
+  static TextStyle get bodyXSBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w700,
+        fontSize: 10,
+        height: 1.36,
+        letterSpacing: 0.003,
+      );
+  static TextStyle get bodyXSSemiBold => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 10,
+        height: 1.36,
+        letterSpacing: 0.003,
+      );
+  static TextStyle get bodyXSRegular => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w400,
+        fontSize: 10,
+        height: 1.36,
+        letterSpacing: 0.003,
+      );
+  static TextStyle get buttonsLarge => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 17,
+        height: 1.5,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get buttonsMedium => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 15,
+        height: 1.46,
+        letterSpacing: -0.02,
+      );
+  static TextStyle get buttonsSmall => const TextStyle(
+        fontFamily: 'Open Sans',
+        fontWeight: FontWeight.w600,
+        fontSize: 13,
+        height: 1.36,
+        letterSpacing: -0.02,
+      );
 }
