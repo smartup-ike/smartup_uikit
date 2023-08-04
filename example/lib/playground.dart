30d6737eafdcf7104688023adc6acb96401067f1
Gorianm <gorian@smartupweb.com>
2023-08-04 14:51:34 +0300

    ghostIconButton bug fix
    Fixed a bug in ghostIconButton where a black color would flicker on hover. It was the transition of the animated container inside UIKitbutton. The solution was to modify the theme of ghostIconButton and add a color not exactly transparent but very close to it.

--- example/lib/playground.dart
+++ example/lib/playground.dart
@@ -8,17 +8,6 @@ class Playground extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final optionsMap = {
-      1: "Option 1",
-      2: "Option 2",
-      3: "Option 3",
-      4: "Option 4"
-    };
-    final text$ = useState("Please pick an Option");
-    final selectedValue$ = useState(1);
-
-    final TextEditingController inputController = useTextEditingController();
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: UIKitTheme.of(context).colors.primary800,
@@ -27,96 +16,13 @@ class Playground extends HookWidget {
       body: Center(
         child: Column(
           children: [
-            const SizedBox(height: 50),
-            Text(
-              "You picked: ${text$.value}",
-              style: UIKitTheme.of(context).typography.headings3Bold,
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 400,
-              child: UIKitDropdownButton.mediumFilled(
-                label: const Text("Pick an Option"),
-                input: Text(selectedValue$.value.toString()),
-                isDisabled: false,
-                trailing: const Icon(Icons.add_circle),
-                onTap: (position, size) async {
-                  selectedValue$.value = await Navigator.of(context).push(
-                        UIKitDropdownRoute(
-                          child: OptionsSelectorDialog(
-                            initialValue: selectedValue$.value,
-                            optionsMap: optionsMap,
-                          ),
-                          position: position,
-                          size: size,
-                        ),
-                      ) ??
-                      selectedValue$.value;
-                  // if (selectedValue$.value !=null) {
-                  //   selectedValue$.value = ;
-                  // }
-                  text$.value = selectedValue$.value.toString();
-                },
-              ),
-            ),
-            const SizedBox(height: 40),
-            UIKitTextInput.largeLineInput(
-              controller: inputController,
-              keyboardType: const TextInputType.numberWithOptions(
-                signed: true,
-                decimal: true,
-              ),
-              inputFormatters: [
-                CurrencyTextInputFormatter(
-                  locale: 'el_GR',
-                  symbol: '€',
-                ),
-              ],
+            SizedBox(height: 50),
+            UIKitIconButton.largeGhost(
+              icon: Icon(Icons.account_box_rounded),
+              onTap: () {},
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class OptionsSelectorDialog extends HookWidget {
-  const OptionsSelectorDialog({
-    super.key,
-    this.initialValue,
-    this.optionsMap,
-  });
-
-  final int? initialValue;
-  final Map<int, String>? optionsMap;
-
-  @override
-  Widget build(BuildContext context) {
-    final selectedValue$ = useState(initialValue);
-
-    return UIKitDropdownMenuSingleSelect<int?>(
-      initialValue: initialValue,
-      value: selectedValue$.value,
-      onChange: (newValue) {
-        selectedValue$.value = newValue;
-        // Navigator.of(context).pop(selectedValue$.value);
-      },
-      itemTrailing: const Icon(Icons.add_circle),
-      actions: [
-        UIKitButton.smallOutline(
-          labelText: const Text('Ακύρωση'),
-          onTap: () => Navigator.of(context).pop(initialValue),
-        ),
-        const SizedBox(width: 8),
-        UIKitButton.smallPrimary(
-          labelText: const Text('Εντάξει'),
-          onTap: () => Navigator.of(context).pop(selectedValue$.value),
-        ),
-      ],
-      options: optionsMap!.keys.toList(),
-      labels: List.generate(
-        optionsMap!.keys.toList().length,
-        (index) => Text(optionsMap!.values.toList()[index]),
       ),
     );
   }
