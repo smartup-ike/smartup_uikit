f1a6dee83d82459ce7ff202fb28d5b8d51936672
StratosKyriazidis <32573946+StratosKyriazidis@users.noreply.github.com>
2024-03-28 11:15:27 +0200

    pub upgrade

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gorianm/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gorianm/Documents/GitHub/smartup_uikit/example"
+export "FLUTTER_ROOT=/Users/stratoskyriazidis/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/stratoskyriazidis/Documents/GitHub/smartup_uikit/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
